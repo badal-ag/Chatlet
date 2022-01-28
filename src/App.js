46092f1c834c27f3e00cf8b5142f950a159e1e36
Badal Agarwal <badal.agarwal1369@gmail.com>
2022-01-28 20:58:52 +0530

    ChatEngine Integrated and Layout Formatted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ChatEngine
+          height="100vh"
+          projectID="7509e767-5964-4595-9688-edcd5d547654"
+          userName="badal"
+          userSecret="123123"
+          renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+      />
+    </>
   );
 }
 
